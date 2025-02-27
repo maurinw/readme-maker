f7e2538412b2528147c1a0c7cccf330b127ce5e9
maurinw <dev.maurin@gmail.com>
2025-02-27 16:29:08 +0100

    basic html layout

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const path = require('path');
-const express = require('express');
-const fs = require('fs');
+const path = require("path");
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Serve static files from the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(4000, () => console.log('Server läuft auf Port 4000'));
+// Serve Bootstrap files from node_modules
+app.use(
+  "/bootstrap",
+  express.static(path.join(__dirname, "node_modules", "bootstrap", "dist"))
+);
+
+// Start the server
+app.listen(4000, () => console.log("Server läuft auf Port 4000"));
