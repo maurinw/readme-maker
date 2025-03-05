2a04e0658bb23896f1f7c7c13a676dfab0b85406
maurinw <dev.maurin@gmail.com>
2025-03-05 14:55:58 +0100

    initial commit and project setup

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/auth');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Use express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Session Middleware hinzufügen
+app.use(session({
+  secret: 'shhhh, very secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Statische Dateien
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/showdown', express.static(path.join(__dirname, 'node_modules/showdown/dist')));
+
+app.use('/', indexRouter);
+app.use('/auth', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page using error.ejs and provide a title
+  res.status(err.status || 500);
+  res.render('error', { title: 'Error' });
+});
+
+module.exports = app;
