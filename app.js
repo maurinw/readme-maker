7e56f5b2fd13cb8f77fdce91934fd19892b2338d
maurinw <dev.maurin@gmail.com>
2025-03-10 13:23:15 +0100

    update navbar and use icon for dropdown

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts');
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Use express-ejs-layouts
+// Use express-ejs
 app.use(expressLayouts);
 app.set('layout', 'layout');
 
@@ -34,7 +34,8 @@ app.use(session({
 // Statische Dateien
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
-app.use('/showdown', express.static(path.join(__dirname, 'node_modules/showdown/dist')));
+app.use('/bootstrap-icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')));
+app.use('/showdown', express.static(path.join(__dirname, 'node_modules/showdown/font')));
 
 app.use('/', indexRouter);
 app.use('/auth', usersRouter);
@@ -46,11 +47,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page using error.ejs and provide a title
   res.status(err.status || 500);
   res.render('error', { title: 'Error' });
 });
